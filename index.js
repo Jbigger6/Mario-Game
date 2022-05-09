d6c5c0172ef6e04707d237a2593de365e29374ed
Joseph Bigger <joseph.bigger@novusmedia.com>
2022-05-09 00:50:28 -0500

    Updates

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+import platform from './images/platform.png'
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const gravity = 0.5;
+const gravity = .5;
 
 class Player {
     constructor() {
         // sets players initial x & y positions within the canvas
         this.position = {
-            x: 100,
-            y: 100
+            x: canvas.width * .1,
+            y: 50
         };
 
         // sets players initial x & y velocity
@@ -37,7 +39,7 @@ class Player {
         this.draw()
         this.position.y += this.velocity.y;
         this.position.x += this.velocity.x;
-        if (player.position.y +this.height + this.velocity.y <= canvas.height) {
+        if (player.position.y + this.height + this.velocity.y <= canvas.height) {
             this.velocity.y += gravity;
         }
         else {
@@ -47,15 +49,25 @@ class Player {
 }
 
 class Platform {
-    constructor() {
-        /** 
-        * left off at 36:00 mark of video
-        * https://www.youtube.com/watch?v=4q2vvZn5aoo&t=3438s&ab_channel=ChrisCourses
-        */
+    constructor({ x, y }) {
+        this.position = {
+            x,
+            y
+        }
+
+        this.width = 200,
+            this.height = 20
+    }
+
+    draw() {
+        context.fillStyle = 'blue';
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
 const player = new Player(); // creates new player object
+const platforms = [new Platform({ x: 200, y: 500 }), new Platform({ x: 500, y: 400 })]; // creates new platform object
+
 const keys = {
     right: {
         pressed: false
@@ -64,6 +76,8 @@ const keys = {
         pressed: false
     }
 };
+
+let scrollOffset = 0;
 
 /**
  * animate function is constantly calling itself which calls "player.update"
@@ -77,15 +91,55 @@ function animate() {
     requestAnimationFrame(animate)
     context.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw();
+    });
 
-    if (keys.right.pressed) {
+    // player horizontal movement
+    if (
+        keys.right.pressed &&
+        player.position.x < canvas.width * .4
+    ) {
         player.velocity.x = 5;
     }
-    else if (keys.left.pressed) {
+    else if (
+        keys.left.pressed &&
+        player.position.x > canvas.width * .1
+    ) {
         player.velocity.x = -5;
     }
     else {
         player.velocity.x = 0;
+
+        // moves platforms horizontally when player reaches boundaries
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                scrollOffset += 5;
+                platform.position.x -= 5;
+            });
+        }
+        if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                scrollOffset -= 5;
+                platform.position.x += 5;
+            });
+        }
+    }
+
+    // player + platform collision detection
+    platforms.forEach((platform) => {
+        if (
+            player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width
+        ) {
+            player.velocity.y = 0;
+        }
+    });
+
+    if (scrollOffset >= 2000) {
+        console.log('You win!!')
     }
 }
 
@@ -112,17 +166,17 @@ window.addEventListener('keydown', (event) => {
         case 'KeyA':
             keys.left.pressed = true;
             break;
-        
-        case 'KeyS':
-            player.velocity.y += 10;
-            break;
-        
+
+        // case 'KeyS':
+        //     player.velocity.y += 10;
+        //     break;
+
         case 'KeyD':
             keys.right.pressed = true;
             break;
 
         case 'Space':
-            player.velocity.y -= 10;
+            player.velocity.y -= 20;
             break;
     }
 });
@@ -132,17 +186,9 @@ window.addEventListener('keyup', (event) => {
         case 'KeyA':
             keys.left.pressed = false;
             break;
-        
-        case 'KeyS':
-            player.velocity.y += 10;
-            break;
-        
+
         case 'KeyD':
             keys.right.pressed = false;
-            break;
-
-        case 'Space':
-            player.velocity.y -= 10;
             break;
     }
 });
